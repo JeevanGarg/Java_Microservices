8d7a2af8c6e51c42859ac0590053f92d694e55cc
JeevanGarg <luckyjeevangarg89@gmail.com>
2024-06-18 22:45:47 +0530

    Microservices

--- DepartmentService/src/main/java/com/DepartmentService/DepartmentServiceApplication.java
+++ DepartmentService/src/main/java/com/DepartmentService/DepartmentServiceApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DepartmentServiceApplication {
-
-	public static void main(String[] args) {
+public class DepartmentServiceApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
